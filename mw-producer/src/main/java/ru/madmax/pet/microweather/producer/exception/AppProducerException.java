9f4fc9f5f73209d41c54e4734bc7223dc0e08c4e
madmax001 <madmax001@ya.ru>
2023-11-24 18:28:06 +0400

    Producer: AppException: constructor overload

--- mw-producer/src/main/java/ru/madmax/pet/microweather/producer/exception/AppProducerException.java
+++ mw-producer/src/main/java/ru/madmax/pet/microweather/producer/exception/AppProducerException.java
@@ -1,9 +1,12 @@
 package ru.madmax.pet.microweather.producer.exception;
 
-import java.net.SocketException;
 
 public class AppProducerException extends RuntimeException {
-    public AppProducerException(SocketException e) {
+    public AppProducerException(Exception e) {
         super(e);
+    }
+
+    public AppProducerException(String message) {
+        super(message);
     }
 }
