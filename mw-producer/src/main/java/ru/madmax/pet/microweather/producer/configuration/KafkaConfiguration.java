466896ca69c7bcad9c4b6273dd71a1c9432a3044
madmax001 <madmax001@ya.ru>
2023-12-03 17:53:09 +0400

    Producer: remove unused

--- mw-producer/src/main/java/ru/madmax/pet/microweather/producer/configuration/KafkaConfiguration.java
+++ mw-producer/src/main/java/ru/madmax/pet/microweather/producer/configuration/KafkaConfiguration.java
@@ -1,13 +1,9 @@
 package ru.madmax.pet.microweather.producer.configuration;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 @EnableKafka
-@RequiredArgsConstructor
-public class KafkaConfiguration {                                   //todo надо ли?
-    private final ProducerFactory<Object, Object> producerFactory;
+public class KafkaConfiguration {
 }
