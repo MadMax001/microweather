6cab42435a18dc6726c2c322c09e40ee6b9fc398
073BodrovMB <madmax001@ya.ru>
2023-12-05 17:00:06 +0400

    Producer: Refactoring

--- mw-producer/src/test/java/ru/madmax/pet/microweather/producer/PLauncherTest.java
+++ mw-producer/src/test/java/ru/madmax/pet/microweather/producer/PLauncherTest.java
@@ -1,12 +1,8 @@
 package ru.madmax.pet.microweather.producer;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PLauncherTest {
