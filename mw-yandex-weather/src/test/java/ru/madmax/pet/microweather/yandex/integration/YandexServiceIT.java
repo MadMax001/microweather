0e2b62840020eaae87cb7ea0ab985f0bb543be7d
073BodrovMB <madmax001@ya.ru>
2023-12-21 11:18:18 +0400

    Yandex: Integration tests

---
+++ mw-yandex-weather/src/test/java/ru/madmax/pet/microweather/yandex/integration/YandexServiceIT.java
@@ -0,0 +1,175 @@
+package ru.madmax.pet.microweather.yandex.integration;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.test.StepVerifier;
+import ru.madmax.pet.microweather.common.model.Point;
+import ru.madmax.pet.microweather.common.model.TestPointBuilder;
+import ru.madmax.pet.microweather.common.model.TestWeatherBuilder;
+import ru.madmax.pet.microweather.common.model.Weather;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "app.weather.timeout=1000",
+        "app.weather.url=http://localhost:44444",
+        "app.weather.path=/test",
+        "app.weather.retry.attempts=1"
+})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class YandexServiceIT {
+    private final WebTestClient webTestClient;
+    MockWebServer remoteMockServer;
+    String remoteYandexURL;
+    final static String SERVICE_LOCAL_PATH = "/api/v1/weather";
+    final static String GUID_HEADER_KEY = "request-guid";
+    final static String GUID_HEADER_VALUE = "testguid";
+    final static String ERROR_HEADER_KEY = "request-error";
+    @BeforeEach
+    void initialize() throws IOException {
+        remoteMockServer = new MockWebServer();
+        remoteMockServer.start(44444);
+        remoteYandexURL = remoteMockServer.url("/test").toString();
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        if (remoteMockServer != null)
+            remoteMockServer.shutdown();
+    }
+
+    @Test
+    void requestWeather_happyPass() throws JsonProcessingException {
+        var objectMapper = new ObjectMapper();
+        final Weather weather = TestWeatherBuilder.aWeather().build();
+        final String stringContent = objectMapper.writeValueAsString(weather);
+
+        final Point point = TestPointBuilder.aPoint().build();
+        final String stringPoint = objectMapper.writeValueAsString(point);
+
+        setMockResponseFromServer(500, stringContent);
+
+        var receivedResponseEntityContent = webTestClient
+                .post()
+                .uri(SERVICE_LOCAL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(stringPoint))
+                .header(GUID_HEADER_KEY, GUID_HEADER_VALUE)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals(GUID_HEADER_KEY, GUID_HEADER_VALUE)
+                .returnResult(Weather.class)
+                .getResponseBody();
+
+        StepVerifier.create(receivedResponseEntityContent)
+                .expectNext(weather)
+                .expectComplete()
+                .verify();
+
+    }
+
+    @Test
+    void requestWeather_butRemoteServiceNotResponseDuringTimeoutValue_AndCheckStatusAndErrorHeader() throws JsonProcessingException {
+        var objectMapper = new ObjectMapper();
+        final Weather weather = TestWeatherBuilder.aWeather().build();
+        final String stringContent = objectMapper.writeValueAsString(weather);
+
+        final Point point = TestPointBuilder.aPoint().build();
+        final String stringPoint = objectMapper.writeValueAsString(point);
+
+        setMockResponseFromServer(1500, stringContent);
+
+        var receivedResponseEntityContent = webTestClient
+                .post()
+                .uri(SERVICE_LOCAL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(stringPoint))
+                .header(GUID_HEADER_KEY, GUID_HEADER_VALUE)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is5xxServerError()
+                .expectHeader().valueEquals(GUID_HEADER_KEY, GUID_HEADER_VALUE)
+                .expectHeader().exists(ERROR_HEADER_KEY)
+                .returnResult(Void.class)
+                .getResponseBody();
+
+        StepVerifier.create(receivedResponseEntityContent)
+                .expectComplete()
+                .verify();
+
+    }
+
+    @Test
+    void requestWeather_withWrongParams_AndCheckStatusAndErrorHeader() throws JsonProcessingException {
+        var objectMapper = new ObjectMapper();
+        final Weather weather = TestWeatherBuilder.aWeather().build();
+        final String stringContent = objectMapper.writeValueAsString(weather);
+
+        final String stringPoint = "{\"lon\":46.001373, \"lat\":\"51A\"}";
+
+        setMockResponseFromServer(500, stringContent);
+
+        var receivedResponseEntityContent = webTestClient
+                .post()
+                .uri(SERVICE_LOCAL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(stringPoint))
+                .header(GUID_HEADER_KEY, GUID_HEADER_VALUE)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is4xxClientError()
+                .expectHeader().valueEquals(GUID_HEADER_KEY, GUID_HEADER_VALUE)
+                .expectHeader().exists(ERROR_HEADER_KEY)
+                .returnResult(Void.class)
+                .getResponseBody();
+
+        StepVerifier.create(receivedResponseEntityContent)
+                .expectComplete()
+                .verify();
+
+    }
+
+    private void setMockResponseFromServer(int timeout, String responseContentString) {
+        Dispatcher dispatcher = new Dispatcher() {
+            @NotNull
+            @Override
+            public MockResponse dispatch(@NotNull RecordedRequest request) {
+                if ("GET".equals(request.getMethod()) &&
+                        request.getPath() != null &&
+                        request.getPath().startsWith("/test")) {
+                    return new MockResponse()
+                            .addHeader("Content-Type", MediaType.APPLICATION_JSON)
+                            .setBody(responseContentString)
+                            .setBodyDelay(timeout, TimeUnit.MILLISECONDS);
+                }
+                return new MockResponse().setResponseCode(404);
+            }
+        };
+
+        remoteMockServer.setDispatcher(dispatcher);
+
+    }
+
+}
