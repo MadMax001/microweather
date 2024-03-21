c1b4f1184921f965c216a2e89f851b7682915f02
max <madmax001@yandex.ru>
2024-03-21 17:43:20 +0400

    Producer: delete unused classes

--- mw-producer/src/main/java/ru/madmax/pet/microcurrency/producer/service/WeatherService.java
+++
@@ -1,8 +0,0 @@
-package ru.madmax.pet.microcurrency.producer.service;
-
-import reactor.core.publisher.Mono;
-import ru.madmax.pet.microcurrency.producer.model.RequestDTO;
-
-public interface WeatherService {
-    Mono<String> registerRequest(RequestDTO request);
-}
