3a7c07e51ad25df351ff8f9feaf6f1279e5e71fd
max <madmax001@yandex.ru>
2024-03-22 17:25:11 +0400

    Yandex: remove module

--- mw-yandex-weather/src/main/java/ru/madmax/pet/microweather/yandex/service/WeatherLoaderService.java
+++
@@ -1,9 +0,0 @@
-package ru.madmax.pet.microweather.yandex.service;
-
-import reactor.core.publisher.Mono;
-import ru.madmax.pet.microweather.common.model.Point;
-import ru.madmax.pet.microweather.common.model.Weather;
-
-public interface WeatherLoaderService {
-    Mono<Weather> requestWeatherByPoint(Point point);
-}
